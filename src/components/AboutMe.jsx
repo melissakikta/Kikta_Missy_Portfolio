0108c3b7d656b6b2cf89743bbfaaeb5544bd9b47
Mrs. Kikta <melissakikta@gmail.com>
2025-03-06 22:38:21 -0500

    updated about me and skills

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -21,7 +21,7 @@ function AboutMe() {
           <h1>About Me</h1>
       {/* Bio paragraph */}
           <p>
-          Full Stack Web Developer (or other title of interest) seeking a career opportunity with a company that values excellence and leadership through service. After a successful tenure in public education, I am excited to transition into software development, bringing my strengths in problem-solving, project management, and collaboration to the tech industry.
+          Full Stack Web Developer seeking a career opportunity with a company that values excellence and leadership through service. After a successful tenure in public education, I am excited to transition into software development, bringing my strengths in problem-solving, project management, and collaboration to the tech industry.
           </p>
           <p>
           With a passion for user-centered design and intuitive interfaces, I thrive on analyzing requirements, developing solutions, and refining applications to enhance user experiences. My ability to communicate effectively, adapt to new challenges, and work across teams makes me a strong asset in full-stack development. I look forward to contributing to a dynamic team where I can apply my technical skills and continue learning while building impactful software that supports both customers and employees.
